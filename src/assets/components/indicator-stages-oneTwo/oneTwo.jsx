6494da173686374944218c3aa0aa7ec00b9995c1
tac morales <tac.yon@genstudents.org>
2025-01-28 09:05:24 -0300

    Estructura para React, y división de los componentes.

---
+++ src/assets/components/indicator-stages-oneTwo/oneTwo.jsx
@@ -0,0 +1,12 @@
+const IndicatorStagesOneTwo = () => {
+  <div class="row mb-3">
+    <div class="col bg-success-subtle opacity-50 border border-success-subtle text fs-4">
+      1 Seleccionar
+    </div>
+    <div class="col bg-success opacity-50 border border-success-subtle text-white text fs-4">
+      2 Check Out
+    </div>
+  </div>;
+};
+
+export default IndicatorStagesOneTwo;
