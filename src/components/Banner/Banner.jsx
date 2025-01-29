9f19df1707bf08ebdd5d51d446fd7b6ecf391b1d
Tamara Zamorano <tamara.zamorano@genstudents.org>
2025-01-29 16:05:11 -0300

    banner sin video funcional

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,60 +1,33 @@
+import React from 'react';
 import './Banner.css';
+import videoSrc from './src/assets/videos/Chelenko-Lodge-Intro-by-ICS.mp4'
 const Banner = () => {
   return (
     <section className="container-fluid banner position-relative vh-100 overflow-hidden">
+      {/* Capa de oscurecimiento*/}
       <div className="position-absolute top-0 start-0 w-100 h-100 object-fit-cover bg-overlay"></div>
-      <video
-        autoPlay
-        muted
-        loop
-        playsInline
-        className="position-absolute top-0 start-0 w-100 h-100 object-fit-cover"
-      >
+      {/*Video de fondo*/}
+      <video autoPlay muted loop playsInline>
         <source
-          src="./src/videos/Chelenko-Lodge-Intro-by-ICS.mp4"
+          src={videoSrc}
           type="video/mp4"
         />
       </video>
+      {/*Contenido banner*/}
       <div className="d-flex justify-content-center align-items-center position-relative z-index-1 h-100">
-        <div
-          className="text-center text-white p-4 d-flex justify-content-center align-items-center flex-column"
-          style={{
-            border: "5px solid #fff",
-            width: "80%",
-            height: "65%",
-            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)"
-          }}
-        >
+        <div className="text-center text-white p-4 d-flex flex-column align-items-center banner-content">
           <img
-            src="./src/img/Logo home.png"
+            src="./src/assets/images/Logo home.png"
             alt="Logo blanco Chelenko"
-            className="fluid mb-4"
-            style={{
-              maxHeight: "150px",
-              objectFit: "contain"
-            }}
+            className="fluid mb-3"
           />
-          <h5
-            className="fw-light text-wrap"
-            style={{
-              textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
-            }}
-          >
+          <h5 className="fw-light text-wrap">
             Creamos experiencias de viaje significativas y memorables,
-            inspiradas
-            <br />
-            por la autenticidad, la hospitalidad y un profundo sentido de
-            conexión
-            <br />
-            con la naturaleza.
+            inspiradas por la autenticidad, la hospitalidad y un profundo sentido de
+            conexión con la naturaleza.
           </h5>
           <p className="lead mb-4 p-4">Descubre la diferencia</p>
-          <a
-            href="./checkout.html"
-            className="btn btn-primary btn-lg border-0 text-white rounded-pill"
-          >
-            Reserva aquí
-          </a>
+          <a href="./checkout.html" className="btn btn-primary btn-lg border-0 text-white rounded-pill btn-reserva">Reserva aquí</a>
         </div>
       </div>
     </section>
