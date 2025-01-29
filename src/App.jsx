896740314574dc490342aa1cc63fa824ab820419
tac morales <tac.yon@genstudents.org>
2025-01-29 00:25:15 -0300

    Se reordena la estructura. Navbar casiok

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
 import "./customBootstrap.css";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home/Home.jsx";
+import Checkout from "./views/Checkout/Checkout.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: '/checkout',
+      element: <Checkout />
+    }
+  ])
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<h1>Bienvenidos</h1>}/>
-          <Route path="/reservar" element={<h1>Reservar</h1>}/>
-          <Route path="/checkout" element={<h1>Checkout</h1>}/>
-          <Route path="/confirmacion-reserva" element={<h1>Reserva Exitosa</h1>}/>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <RouterProvider router={router} />
+      // <BrowserRouter>
+      // <Navbar isUponBanner={true}/>
+      //   <Routes>
+      //     <Route path="/" element={<Home />} />
+      //     <Route path="/reservar" element={<Checkout />} />
+      //     <Route path="/checkout" element={<h1>Checkout el correcto</h1>} />
+      //     <Route
+      //       path="/confirmacion-reserva"
+      //       element={<h1>Reserva Exitosa</h1>}
+      //     />
+      //   </Routes>
+      // </BrowserRouter>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
