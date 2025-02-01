c560eae09efc904664d386a15a84575a87c4b1d0
tac morales <tac.yon@genstudents.org>
2025-02-01 13:13:49 -0300

    Estructura en blanco en Checkout

--- src/views/Checkout/Checkout.jsx
+++ src/views/Checkout/Checkout.jsx
@@ -1,14 +1,14 @@
 import "./Checkout.css";
 import Header from "../../components/Header/Header";
-import Banner from "../../components/Banner/Banner";
-import SectionImgTxt from "../../components/Section-img-txt/Section_img_txt";
+import Footer from "../../components/Footer/Footer";
+
 
 const Checkout = () => {
   return (
     <>
       <Header />
-      <Banner />
-      <SectionImgTxt />
+      
+      <Footer />
     </>
   );
 };
